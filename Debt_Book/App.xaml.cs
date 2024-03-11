9c11c476884caf97074380d68bbd420e278d11dd
Kasper Bruun Bentsen <202103249@post.au.dk>
2024-03-11 12:08:03 +0100

    Add project files.

---
+++ Debt_Book/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Debt_Book
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
