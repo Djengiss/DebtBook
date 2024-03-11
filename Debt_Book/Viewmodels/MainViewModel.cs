9ddd0ca880da0215eab21eca7c116fa47abe67ee
Kasper Bentsen <kasperbentsen1992@hotmail.com>
2024-03-11 17:02:42 +0100

    Updated the code in the MainViewModel

--- Debt_Book/Viewmodels/MainViewModel.cs
+++ Debt_Book/Viewmodels/MainViewModel.cs
@@ -1,24 +1,40 @@
 ﻿using Debt_Book.Views;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Debt_Book.Models;
+using Debt_Book.Services;
 
 namespace Debt_Book.Viewmodels
 {
     internal class MainViewModel : ViewModelBase
     {
-        
+        public ObservableCollection<Debtor> Debtors { get; set; } = new();
         public ICommand AddNewDebtorCommand { get; set; }
         public ICommand ViewDebtorInfoCommand { get; set; }
+        private readonly DebtDatabase _database;
 
         public MainViewModel()
         {
+            _database = new DebtDatabase();
             AddNewDebtorCommand = new Command(async () => await AddDebtor());
             ViewDebtorInfoCommand = new Command(async () => await ViewDebtorInfo());
+            _ = Initialize();
+        }
+
+        private async Task Initialize()
+        {
+            var debtors = await _database.GetDebtors();
+
+            foreach(var debtor in debtors)
+            {
+                Debtors.Add(debtor);
+            }
         }
 
         private async Task AddDebtor()
