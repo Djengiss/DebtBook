100e16160cb04b1517c40f10f8bab74ca4585761
Kasper Bruun Bentsen <202103249@post.au.dk>
2024-03-11 13:48:45 +0100

    Implemented database code

--- Debt_Book/Services/DebtDatabase.cs
+++ Debt_Book/Services/DebtDatabase.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Debt_Book.Models;
 
 
 // Responsible for interacting with the SQLite database.
@@ -11,5 +13,59 @@ namespace Debt_Book.Services
 {
     internal class DebtDatabase
     {
+        private readonly SQLiteAsyncConnection _connection;
+
+        public DebtDatabase()
+        {
+            var dataDir = FileSystem.AppDataDirectory;
+            var databasePath = Path.Combine(dataDir, "DebtData.db");
+
+            string _dbEncryptionKey = SecureStorage.GetAsync("dbKey").Result;
+
+            if (string.IsNullOrEmpty(_dbEncryptionKey))
+            {
+                Guid g = new Guid();
+                _dbEncryptionKey = g.ToString();
+                SecureStorage.SetAsync("dbKey", _dbEncryptionKey);
+            }
+
+            var dbOptions = new SQLiteConnectionString(databasePath, true, key: _dbEncryptionKey);
+
+            _connection = new SQLiteAsyncConnection(dbOptions);
+
+            _ = Initialize();
+        }
+        private async Task Initialize()
+        {
+            await _connection.CreateTableAsync<Debtor>();
+            await _connection.CreateTableAsync<Debt>();
+        }
+
+        public async Task<List<Debtor>> GetDebtors()
+        {
+            return await _connection.Table<Debtor>().ToListAsync();
+        }
+
+        public async Task<Debt> GetDebt(int id)
+        {
+            var query = _connection.Table<Debt>().Where(t => t.Id == id);
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<int> AddDebt(Debt item)
+        {
+            return await _connection.InsertAsync(item);
+        }
+
+        public async Task<int> DeleteDebt(Debt item)
+        {
+            return await _connection.DeleteAsync(item);
+        }
+
+        public async Task<int> UpdateDebt(Debt item)
+        {
+            return await _connection.UpdateAsync(item);
+        }
     }
 }
